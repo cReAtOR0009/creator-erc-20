30029cc28ea0a4082c45c0d1b7b5ae25e52f019e
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-16 00:00:49 +0100

    custom token taking takonName and symbol as constructor now

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -1,13 +1,11 @@
  // SPDX-License-Identifier: MIT
-
- // SPDX-License-Identifier: SEE LICENSE IN LICENSE
  pragma solidity ^0.8.7;
 
  import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
  contract OurToken is ERC20 {
 
-    constructor(uint256 initSupply) ERC20("OurToken", "OT") {
+    constructor(uint256 initSupply, string memory tokenName, string memory tokenSymbol) ERC20(tokenName, tokenSymbol) {
         _mint(msg.sender, initSupply);
     }
  }
