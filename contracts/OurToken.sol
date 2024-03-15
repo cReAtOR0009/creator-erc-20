2acf6a5fd66e2275d465568b8823e7b873325d4c
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-15 23:38:09 +0100

    make contract compilable by hardhat by putting contract files into newly created contract folder

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -3,9 +3,9 @@
  // SPDX-License-Identifier: SEE LICENSE IN LICENSE
  pragma solidity ^0.8.7;
 
- import "@openzeppelin/contracts/token/ERC20/ERC20.sol"
+ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
- contract OurToken is ERC2 {
+ contract OurToken is ERC20 {
 
     constructor(uint256 initSupply) ERC20("OurToken", "OT") {
         _mint(msg.sender, initSupply);
